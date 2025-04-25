e578df6c26fa89d3586fe56f08a15a8f483b7b81
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-25 19:57:49 -0300

    panana

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+Console.Clear();
+
+Console.Write("coloque sua senha: ");
+string senhaDigitda;
+senhaDigitda = Console.ReadLine()!;
+string senhacorreta;
+string x;
+Console.Clear();
+Console.Write("digite sua senha novamente para entrar: ");
+x = Console.ReadLine()!;
+
+senhacorreta = x;
+
+if(senhaDigitda == senhacorreta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Olá usuario, entrada bem sucedida");
+
+
+}
+
+else
+
+{
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Senha Incorreta.");
+
+
+
+}
+Console.ResetColor();
+
